baea665b1b4d9543f21098e47b2e7cb929dbd429
Jaesung Park <jaesung.cs@gmail.com>
2025-08-15 16:30:06 +0900

    add volk, vma from VulkanSDK optional component

--- examples/vkgs_exe.cc
+++ examples/vkgs_exe.cc
@@ -1,6 +1,12 @@
 #include <iostream>
 
+#include "vkgs/module.h"
+
 int main() {
   std::cout << "Hello vkgs" << std::endl;
+
+  vkgs::Module module;
+  std::cout << "Module created" << std::endl;
+
   return 0;
 }
--- src/vkgs/module.cc
+++ src/vkgs/module.cc
@@ -1,16 +1,129 @@
 #include "vkgs/module.h"
 
 #include <iostream>
+#include <vector>
+
+#include "volk.h"
 
 namespace vkgs {
+namespace {
+
+VkBool32 debug_callback(VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
+                        VkDebugUtilsMessageTypeFlagsEXT message_type,
+                        const VkDebugUtilsMessengerCallbackDataEXT* callback_data, void* user_data) {
+  const char* level = nullptr;
+  switch (message_severity) {
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:
+      level = "VERBOSE";
+      break;
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
+      level = "WARNING";
+      break;
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
+      level = "ERROR  ";
+      break;
+    default:
+      level = "UNKNOWN";
+      break;
+  }
+
+  const char* type = nullptr;
+  switch (message_type) {
+    case VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT:
+      type = "GENERAL    ";
+      break;
+    case VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT:
+      type = "VALIDATION ";
+      break;
+    case VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT:
+      type = "PERFORMANCE";
+      break;
+    default:
+      type = "UNKNOWN    ";
+      break;
+  }
+
+  std::cerr << "Vulkan Validation [" << level << "] [" << type << "] " << callback_data->pMessage << std::endl;
+  return VK_FALSE;
+}
+
+}  // namespace
 
 class Module::Impl {
  public:
-  Impl() { std::cout << "Impl created" << std::endl; }
+  Impl() {
+    std::cout << "Impl created" << std::endl;
 
-  ~Impl() { std::cout << "Impl destroyed" << std::endl; }
+    volkInitialize();
+
+    // Instance
+    VkApplicationInfo app_info = {VK_STRUCTURE_TYPE_APPLICATION_INFO};
+    app_info.pApplicationName = "vkgs";
+    app_info.applicationVersion = VK_MAKE_VERSION(0, 0, 0);
+    app_info.pEngineName = "vkgs";
+    app_info.engineVersion = VK_MAKE_VERSION(0, 0, 0);
+    app_info.apiVersion = VK_API_VERSION_1_4;
+
+    VkDebugUtilsMessengerCreateInfoEXT messenger_info = {VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT};
+    messenger_info.messageSeverity =
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+    messenger_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+                                 VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+                                 VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+    messenger_info.pfnUserCallback = &debug_callback;
+
+    std::vector<const char*> validation_layers = {
+        "VK_LAYER_KHRONOS_validation",
+    };
+
+    std::vector<const char*> extensions = {
+        VK_EXT_DEBUG_UTILS_EXTENSION_NAME,
+    };
+
+    VkInstanceCreateInfo instance_info = {VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO};
+    instance_info.pNext = &messenger_info;
+    instance_info.pApplicationInfo = &app_info;
+    instance_info.enabledLayerCount = validation_layers.size();
+    instance_info.ppEnabledLayerNames = validation_layers.data();
+    instance_info.enabledExtensionCount = extensions.size();
+    instance_info.ppEnabledExtensionNames = extensions.data();
+    vkCreateInstance(&instance_info, NULL, &instance_);
+
+    volkLoadInstance(instance_);
+
+    vkCreateDebugUtilsMessengerEXT(instance_, &messenger_info, NULL, &messenger_);
+
+    // Physical device
+    uint32_t physical_device_count = 0;
+    vkEnumeratePhysicalDevices(instance_, &physical_device_count, NULL);
+    std::vector<VkPhysicalDevice> physical_devices(physical_device_count);
+    vkEnumeratePhysicalDevices(instance_, &physical_device_count, physical_devices.data());
+    physical_device_ = physical_devices[0];
+
+    VkPhysicalDeviceProperties device_properties;
+    vkGetPhysicalDeviceProperties(physical_device_, &device_properties);
+    std::cout << "Physical device: " << device_properties.deviceName << std::endl;
+
+    // device
+    VkDeviceCreateInfo device_info = {VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO};
+    device_info.queueCreateInfoCount = 0;
+    device_info.pQueueCreateInfos = NULL;
+    device_info.enabledExtensionCount = 0;
+    device_info.ppEnabledExtensionNames = NULL;
+    vkCreateDevice(physical_device_, &device_info, NULL, &device_);
+  }
+
+  ~Impl() {
+    std::cout << "Impl destroyed" << std::endl;
+
+    volkFinalize();
+  }
 
  private:
+  VkInstance instance_ = VK_NULL_HANDLE;
+  VkDebugUtilsMessengerEXT messenger_ = VK_NULL_HANDLE;
+  VkPhysicalDevice physical_device_ = VK_NULL_HANDLE;
+  VkDevice device_ = VK_NULL_HANDLE;
 };
 
 Module::Module() : impl_(std::make_unique<Impl>()) {}
