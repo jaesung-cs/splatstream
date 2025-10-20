826354c1ffd2c87d97c46865f9f4b3fac89fd0d6
Jaesung Park <jaesung.cs@gmail.com>
2025-10-20 23:20:05 +0900

    fix static library export macro

--- src/gpu/include/vkgs/gpu/export_api.h
+++ src/gpu/include/vkgs/gpu/export_api.h
@@ -3,10 +3,12 @@
 
 // clang-format off
 #if defined(_WIN32) || defined(__CYGWIN__)
-  #ifdef VKGS_GPU_EXPORTS
+  #if defined(VKGS_GPU_STATIC)
+    #define VKGS_GPU_API
+  #elif defined(VKGS_GPU_EXPORTS)
     #define VKGS_GPU_API __declspec(dllexport)
   #else
-    #define VKGS_GPU_API __declspec(dllimport)
+    #define VKGS_GPU_API __declspec(dllimport)  
   #endif
 #else
   #define VKGS_GPU_API
