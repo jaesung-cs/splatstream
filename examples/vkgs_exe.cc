1c7528f6739f3671daf8f358cfb22f7e48e62dbd
Jaesung Park <jaesung.cs@gmail.com>
2025-10-20 16:03:00 +0900

    build example

--- examples/vkgs_exe.cc
+++ examples/vkgs_exe.cc
@@ -1,8 +1,6 @@
 #include <iostream>
 
 #include "vkgs/module.h"
-#include "vkgs/gaussian_splats.h"
-#include "vkgs/rendered_image.h"
 
 int main() {
   std::cout << "Hello vkgs" << std::endl;
@@ -12,10 +10,6 @@ int main() {
   std::cout << "graphics queue index: " << module.graphics_queue_index() << std::endl;
   std::cout << "compute  queue index: " << module.compute_queue_index() << std::endl;
   std::cout << "transfer queue index: " << module.transfer_queue_index() << std::endl;
-
-  auto gaussian_splats = module.load_from_ply("models/bonsai_30000.ply");
-  std::cout << "loaded gaussian splats" << std::endl;
-  std::cout << "size: " << gaussian_splats.size() << std::endl;
 
   return 0;
 }
